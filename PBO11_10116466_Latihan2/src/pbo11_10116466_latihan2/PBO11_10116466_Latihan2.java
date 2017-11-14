f60fe492d1b57948e2299b17e659982ca46deaca
estertambunan <32484899+estertambunan@users.noreply.github.com>
2017-11-14 17:26:10 +0700

    Add files via upload

---
+++ PBO11_10116466_Latihan2/src/pbo11_10116466_latihan2/PBO11_10116466_Latihan2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11_10116466_latihan2;
+
+/**
+ *
+ * @author user
+ */
+public class PBO11_10116466_Latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
